217a1171f846f283ce2c69058c317735f4be541a
giulianna2043 <giulianna.2043@gmail.com>
2017-05-25 17:07:20 -0700

    falta acomodar footer

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(
+    function(){
 	$("#ocultar-cuadros").on("click",function(){
 
         $("#mostrar-cuadros").show();
@@ -9,5 +10,22 @@ $(document).ready(function(){
         $("#mostrar").show();
         $("#mostrar-cuadros").hide();
 	});
+    $("#general").click(function () {
+        $('#respuesta-ej1').slideToggle("slow");
+    });
 });
+
+/*1
+
+<!-- ejemplo footer -->
+<p>Pregunta... (<a href="#" id="alternar-respuesta-ej1">ver respuesta</a>)
+ 
+<div id="respuesta-ej1" style="display:none">Respuesta... </div>
+
+// jQuery
+$(document).ready(function(){ 
+   $('#alternar-respuesta-ej2').on('click',function(){
+      $('#respuesta-ej2').toggle('slow');
+   });
+});*/
 
